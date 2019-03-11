4a3bd33be78ec6d958ed437a034f326c653d44dc
Roker <roker@pep-project.org>
2019-03-11 13:27:11 +0100

    remove the big switch(), use pep_status_to_string() from pEpEngine.h instead.

--- status_to_string.hh
+++ status_to_string.hh
@@ -5,7 +5,7 @@
 
 namespace pEp
 {
-    // creates a textual string and the numerical status value.
+    // creates a textual string (returned by pep_status_to_string() ) and the numerical status value.
     // in pEpEngine.h positive values are in hex value, negatives in decimal. So we follow this.
     std::string status_to_string(PEP_STATUS status);
     
