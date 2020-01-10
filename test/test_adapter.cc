53beb9ba64b885fcd3daf9bda222776ae6142bd3
heck <heck@pep.foundation>
2020-01-10 13:39:28 +0100

    update test_adapter.cc to use new pEpLog()

--- test/test_adapter.cc
+++ test/test_adapter.cc
@@ -12,30 +12,29 @@ using namespace pEp::Adapter;
 
 PEP_STATUS messageToSend(struct _message *msg)
 {
-    cout << "calling messageToSend();\n";
+    pEpLog("called()");
     return PEP_STATUS_OK;
 }
 
 PEP_STATUS notifyHandshake(pEp_identity *me, pEp_identity *partner, sync_handshake_signal signal)
 {
-    cout << "calling notifyHandshake();\n";
+    pEpLog("called()");
     return PEP_STATUS_OK;
 }
 
 int main()
 {
-    cout << "updating or creating identity for me\n";
+    pEpLog("updating or creating identity for me");
     pEp_identity *me = new_identity("alice@peptest.ch", NULL, "23", "Who the F* is Alice");
     assert(me);
     PEP_STATUS status = myself(session(), me);
     free_identity(me);
     pEp::throw_status(status);
 
-    cout << "starting the adapter including sync\n";
+    pEpLog("starting the adapter including sync");
     startup(messageToSend, notifyHandshake);
     sleep(3);
     shutdown();
 
     return 0;
 }
-
