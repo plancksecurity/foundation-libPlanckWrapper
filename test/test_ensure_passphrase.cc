c0bc63dc8f5f168dcce3427b8937ee2c1f1becde
heck <heck@pep.foundation>
2021-04-24 23:00:09 +0200

    Test: test_ensure_passphrase - remove of unused includes

--- test/test_ensure_passphrase.cc
+++ test/test_ensure_passphrase.cc
@@ -3,21 +3,21 @@
 
 #include "framework/framework.hh"
 
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <unistd.h>
+//#include <iostream>
+//#include <fstream>
+//#include <sstream>
+//#include <unistd.h>
 #include <assert.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/param.h>
+//#include <stdlib.h>
+//#include <string.h>
+//#include <sys/param.h>
 
-#include <pEp/message_api.h>
+//#include <pEp/message_api.h>
 #include <pEp/keymanagement.h>
 #include <pEp/key_reset.h>
 
 #include "../src/passphrase_cache.hh"
-#include "../src/status_to_string.hh"
+//#include "../src/status_to_string.hh"
 #include "../src/Adapter.hh"
 
 using namespace pEp;
