a17dddf71628c0aaaddf0d709d25b0f624531fc7
heck <heck@pep.foundation>
2021-10-17 23:13:47 +0200

    Tests: test_hexbin.cc - we are always in namepace ::pEp

--- test/test_hexbin.cc
+++ test/test_hexbin.cc
@@ -4,7 +4,7 @@
 #include <cassert>
 #include <pEp/pitytest11/PityTest.hh>
 
-using namespace pEp::Utils;
+using namespace pEp;
 
 int main()
 {
@@ -13,26 +13,26 @@ int main()
     {
         // Valid hex string
         std::string str_in{ "FFABCD00EF123200" };
-        std::vector<unsigned char> bin = pEp::Utils::hex2bin(str_in);
+        std::vector<unsigned char> bin = Utils::hex2bin(str_in);
         PITYASSERT(str_in.length() == bin.size() * 2, "Size error");
 
         std::string str_out = pEp::Utils::bin2hex(bin);
-        pEpLog("Hex_IN  : '" + to_lower(str_in) + "'");
-        pEpLog("Hex_OUT : '" + to_lower(str_out) + "'");
+        pEpLog("Hex_IN  : '" + Utils::to_lower(str_in) + "'");
+        pEpLog("Hex_OUT : '" + Utils::to_lower(str_out) + "'");
 
-        PITYASSERT(to_lower(str_in) == to_lower(str_out), "roundtrip failed");
+        PITYASSERT(Utils::to_lower(str_in) == Utils::to_lower(str_out), "roundtrip failed");
     }
 
     {
         // Uneven string throws
         std::string str_in{ "FFA" };
-        PITYASSERT_THROWS(pEp::Utils::hex2bin(str_in), "Uneven string should throw");
+        PITYASSERT_THROWS(Utils::hex2bin(str_in), "Uneven string should throw");
     }
 
     {
         // Non-hex chars
         std::string str_in{ "pEp!" };
-        PITYASSERT_THROWS(pEp::Utils::hex2bin(str_in), "Invalid hex chars should throw");
+        PITYASSERT_THROWS(Utils::hex2bin(str_in), "Invalid hex chars should throw");
     }
 
     pEpLog("All tests passsed");
