c1775a7ad0ee2158a18b855b46f8049f1e01da1e
heck <heck@pep.foundation>
2022-02-22 22:34:58 +0100

    Fix: pEp::Utils to_string() func use ref instead of ptr

--- src/utils.hh
+++ src/utils.hh
@@ -15,21 +15,32 @@
 namespace pEp {
     namespace Utils {
         // C-types to C++ types
-        std::vector<::pEp_identity *> to_cxx(const ::identity_list &idl);
+        std::vector<::pEp_identity*> to_cxx(const ::identity_list& idl);
 
         // pEpEngine datatypes to string
-        std::string to_string(const ::pEp_identity *const ident, bool full = true, int indent = 0);
-        std::string to_string(const ::bloblist_t *const ident, bool full = true, int indent = 0);
-        std::string to_string(const ::stringpair_list_t *const spl, bool full = true, int indent = 0);
-        std::string to_string(const ::message *const msg, bool full = true, int indent = 0);
-        std::string to_string(const ::identity_list *const idl, bool full = true, int indent = 0);
-        std::string to_string(const ::pEp_member *const member, bool full = true, int indent = 0);
-        std::string to_string(const ::member_list *const mbl, bool full = true, int indent = 0);
-        std::string to_string(const ::pEp_group *const group, bool full = true, int indent = 0);
+        std::string to_string(const ::pEp_identity& ident, bool full = true, int indent = 0);
+        std::string to_string(const ::bloblist_t& ident, bool full = true, int indent = 0);
+        std::string to_string(const ::stringpair_list_t& spl, bool full = true, int indent = 0);
+        std::string to_string(const ::message& msg, bool full = true, int indent = 0);
+        std::string to_string(const ::identity_list& idl, bool full = true, int indent = 0);
+        std::string to_string(const ::pEp_member& member, bool full = true, int indent = 0);
+        std::string to_string(const ::member_list& mbl, bool full = true, int indent = 0);
+        std::string to_string(const ::pEp_group& group, bool full = true, int indent = 0);
 
         // Misc
         std::string readKey(); // TODO: Move to std_utils
 
     } // namespace Utils
 } // namespace pEp
+
+// ostream inserters
+std::ostream& operator<<(std::ostream& o, const ::pEp_identity& pEp_c_dt);
+std::ostream& operator<<(std::ostream& o, const ::bloblist_t& pEp_c_dt);
+std::ostream& operator<<(std::ostream& o, const ::stringpair_list_t& pEp_c_dt);
+std::ostream& operator<<(std::ostream& o, const ::message& pEp_c_dt);
+std::ostream& operator<<(std::ostream& o, const ::identity_list& pEp_c_dt);
+std::ostream& operator<<(std::ostream& o, const ::pEp_member& pEp_c_dt);
+std::ostream& operator<<(std::ostream& o, const ::member_list& pEp_c_dt);
+std::ostream& operator<<(std::ostream& o, const ::pEp_group& pEp_c_dt);
+
 #endif // LIBPEPADAPTER_UTILS_HH
