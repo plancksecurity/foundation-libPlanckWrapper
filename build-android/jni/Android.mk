252c2537fc91436d4e232a28cdb80d1923b08175
ignaciogarcia <ig@pep.security>
2023-11-27 12:06:11 +0100

    CORE-230 Update path

--- build-android/jni/Android.mk
+++ build-android/jni/Android.mk
@@ -4,6 +4,7 @@
 # see LICENSE.txt
  
 LOCAL_PATH := $(call my-dir)
+PLANCK_CORE_PATH:=$(LOCAL_PATH)/../../submodules/planckCoreV3
 
 
 include $(CLEAR_VARS)
@@ -15,12 +16,12 @@ LOCAL_CPP_FEATURES += exceptions
 LOCAL_CPPFLAGS += -std=c++14 -DANDROID_STL=c++_shared -frtti
 
 #FIXME: WORKARROUND 
-STUB = $(shell sh $(LOCAL_PATH)/../takeOutHeaderFiles.sh $(LOCAL_PATH)/../../../planckCoreV3/ $(LOCAL_PATH)/../../)
+STUB = $(shell sh $(LOCAL_PATH)/../takeOutHeaderFiles.sh $(PLANCK_CORE_PATH)/ $(LOCAL_PATH)/../../)
 $(info $(STUB))
 
 LIB_PEP_ADAPTER_INCLUDE_FILES := $(wildcard $(LOCAL_PATH)/../../src/*.h*)
 
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../planckCoreV3/build-android/include \
+LOCAL_C_INCLUDES += $(PLANCK_CORE_PATH)/build-android/include \
             $(LIB_PEP_ADAPTER_INCLUDE_FILES:%=%)
 
 LOCAL_EXPORT_C_INCLUDES += $(LOCAL_PATH)../include
