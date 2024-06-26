edfa5058674981fe2322d2915345bd95906ef924
Dirk Zimmermann <dz@planck.security>
2024-06-26 12:13:33 +0200

    CORE-461 Revert "What happens if we cache (email,passphrase)?"
    This reverts commit b35b5fb1480c3e074aeeea7b218ab213a6434841.

--- src/callback_dispatcher.hh
+++ src/callback_dispatcher.hh
@@ -53,7 +53,7 @@ namespace pEp {
             ::pEp_identity *partner,
             ::sync_handshake_signal signal);
 
-        friend const char *PassphraseCache::add(const std::string &account_email, const std::string &passphrase);
+        friend const char *PassphraseCache::add(const std::string &passphrase);
     };
 
     extern CallbackDispatcher callback_dispatcher;
--- src/passphrase_cache.cc
+++ src/passphrase_cache.cc
@@ -9,8 +9,7 @@
 pEp::PassphraseCache pEp::passphrase_cache;
 
 namespace pEp {
-    PassphraseCache::cache_entry::cache_entry(const std::string &e, const std::string& p, time_point t) :
-        account_email{ e, 0, PassphraseCache::cache_entry::max_len },
+    PassphraseCache::cache_entry::cache_entry(const std::string& p, time_point t) :
         passphrase{ p, 0, PassphraseCache::cache_entry::max_len }, tp{ t }
     {
     }
@@ -37,7 +36,7 @@ namespace pEp {
         return *this;
     }
 
-    const char* PassphraseCache::add(const std::string& account_email, const std::string& passphrase)
+    const char* PassphraseCache::add(const std::string& passphrase)
     {
         if (!passphrase.empty()) {
             const char* result = nullptr;
@@ -48,7 +47,7 @@ namespace pEp {
                     _cache.pop_front();
                 }
 
-                _cache.push_back({ account_email, passphrase, clock::now() });
+                _cache.push_back({ passphrase, clock::now() });
                 auto back = _cache.end();
                 assert(!_cache.empty());
                 result = (--back)->passphrase.c_str();
